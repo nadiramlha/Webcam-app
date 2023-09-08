6a404f95f4839140845210f596b3442ce43b1396
nadiramlha <nadiraaamilhanf@gmail.com>
2023-09-08 16:17:30 +0700

    first commit

---
+++ Form1.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Webcam
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection captureDevice;
+        private VideoCaptureDevice videoSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            captureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo deviceList in captureDevice)
+            {
+                comboBoxWebcam.Items.Add(deviceList.Name);
+            }
+            comboBoxWebcam.SelectedIndex = 0;
+            videoSource = new VideoCaptureDevice();
+        }
+
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                pictureBox1.Image = null;
+                pictureBox1.Invalidate();
+            }
+            videoSource = new VideoCaptureDevice(captureDevice[comboBoxWebcam.SelectedIndex].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(VideoSource_NewFrame);
+            videoSource.Start();
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void capturebutton_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = (Bitmap)pictureBox1.Image.Clone();
+        }
+
+        private void saveimagebutton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Save Image As";
+            saveFileDialog.Filter = "image files (*.jpg, *.png) | *.jpg, *.png";
+            ImageFormat imageFormat = ImageFormat.Png;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string ext = System.IO.Path.GetExtension(saveFileDialog.FileName);
+                switch (ext)
+                {
+                    case ".jpg":
+                        imageFormat = ImageFormat.Jpeg;
+                        break;
+                    case ".png":
+                        imageFormat = ImageFormat.Png;
+                        break;
+                }
+                pictureBox2.Image.Save(saveFileDialog.FileName, imageFormat);
+            }
+        }
+
+        private void exitbutton_Click(object sender, EventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                pictureBox1.Image = null;
+                pictureBox1.Invalidate();
+                pictureBox2.Image = null;
+                pictureBox2.Invalidate();
+            }
+            Application.Exit(null);
+        }
+    }
+}
